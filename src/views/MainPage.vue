5496339e5d9c02b7a4dd5cf1830d20605ecc64b0
Amrilazim3 <a.azim0711@gmail.com>
2022-12-04 07:24:20 +0800

    creating MainPage for showing navigation tabs

---
+++ src/views/MainPage.vue
@@ -0,0 +1,39 @@
+<template>
+	<ion-page>
+		<ion-content>
+			<ion-tabs>
+				<ion-router-outlet></ion-router-outlet>
+				<ion-tab-bar slot="bottom">
+					<ion-tab-button tab="explore" href="/app/explore">
+						<ion-icon :icon="mapOutline" />
+						<ion-label>Explore</ion-label>
+					</ion-tab-button>
+
+					<ion-tab-button tab="chat" href="/app/chat">
+						<ion-icon :icon="chatbubblesOutline" />
+						<ion-label>Chats</ion-label>
+					</ion-tab-button>
+
+					<ion-tab-button tab="profile" href="/app/profile">
+						<ion-icon :icon="personCircleOutline" />
+						<ion-label>Profile</ion-label>
+					</ion-tab-button>
+				</ion-tab-bar>
+			</ion-tabs>
+		</ion-content>
+	</ion-page>
+</template>
+
+<script lang="ts" setup>
+import {
+	IonTabBar,
+	IonTabButton,
+	IonTabs,
+	IonContent,
+	IonLabel,
+	IonIcon,
+	IonPage,
+	IonRouterOutlet,
+} from "@ionic/vue";
+import { mapOutline, chatbubblesOutline, personCircleOutline } from "ionicons/icons";
+</script>
