cb781dec66a1132f2caa7aaf0131214897810aea
Amrilazim3 <a.azim0711@gmail.com>
2022-12-05 14:31:33 +0800

    sending data to back-end server

--- src/views/Account/Post/CreatePage.vue
+++ src/views/Account/Post/CreatePage.vue
@@ -9,11 +9,16 @@
 					@submit="save"
 					form-class="$reset"
 					:actions="false"
+					:disabled="isLoading"
 				>
-                    <div class="flex space-x-3 mb-10">
-                        <ion-icon :icon="chevronBackOutline" class="h-5 w-5 self-center" @click="router.back()"></ion-icon>
-                        <h1 class="text-xl font-semibold">New Post</h1>
-                    </div>
+					<div class="flex space-x-3 mb-10">
+						<ion-icon
+							:icon="chevronBackOutline"
+							class="h-5 w-5 self-center"
+							@click="router.back()"
+						></ion-icon>
+						<h1 class="text-xl font-semibold">New Post</h1>
+					</div>
 					<FormKit
 						type="text"
 						name="title"
@@ -33,13 +38,25 @@
 						name="thumbnail"
 						label="Thumbnail"
 						validation="required"
+						accept=".jpg,.png,.pdf"
 					/>
                     <div class="flex justify-between text-sm mb-10">
 						<button @click="router.back()">Cancel</button>
 						<button
-							class="text-white bg-blue-500 py-1 px-4 rounded-lg"
+							class="text-white bg-blue-500 py-2 px-4 rounded-md flex"
 						>
 							Post
+							<template v-if="isLoading">
+								<svg
+									class="animate-spin h-5 w-5 fill-white ml-3"
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 512 512"
+								>
+									<path
+										d="M304 48c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48s48-21.5 48-48zm0 416c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48s48-21.5 48-48zM48 304c26.5 0 48-21.5 48-48s-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48zm464-48c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48s48-21.5 48-48zM142.9 437c18.7-18.7 18.7-49.1 0-67.9s-49.1-18.7-67.9 0s-18.7 49.1 0 67.9s49.1 18.7 67.9 0zm0-294.2c18.7-18.7 18.7-49.1 0-67.9S93.7 56.2 75 75s-18.7 49.1 0 67.9s49.1 18.7 67.9 0zM369.1 437c18.7 18.7 49.1 18.7 67.9 0s18.7-49.1 0-67.9s-49.1-18.7-67.9 0s-18.7 49.1 0 67.9z"
+									/>
+								</svg>
+							</template>
 						</button>
 					</div>
 				</FormKit>
@@ -49,16 +66,46 @@
 </template>
 
 <script lang="ts" setup>
-import {
-	IonPage,
-	IonContent,
-    IonIcon
-} from "@ionic/vue";
-import { useAuthStore } from "@/stores/auth";
+import { IonPage, IonContent, IonIcon, toastController } from "@ionic/vue";
 import { chevronBackOutline } from "ionicons/icons";
 import router from "@/router";
+import { inject, ref } from "vue";
+import { useHeaders } from "@/composables/headers";
+import { useToast } from "@/composables/toast";
 
-const save = () => {
-    console.log('save');
-}
+const axios: any = inject("axios");
+
+const isLoading = ref(false);
+
+const toast = useToast(toastController);
+
+const save = async (data: any, node: any) => {
+	isLoading.value = true;
+
+	const form = new FormData();
+	form.append("title", data.title);
+	form.append("content", data.content);
+	data.thumbnail.forEach((fileItem: any) => {
+		form.append("thumbnail", fileItem.file);
+	});
+
+	try {
+		const response = await axios.post("/user/posts", form, useHeaders());
+
+		if (response.data.status) {
+			isLoading.value = false;
+
+			router.push("/app/explore");
+
+			setTimeout(function () {
+				toast.createToast("Post created", 3000, "top", "primary", "font-semibold text-white");
+			}, 1000);
+		}
+	} catch (error: any) {
+		node.setErrors(
+			[error.response.data.message],
+			error.response.data.errors
+		);
+	}
+};
 </script>
