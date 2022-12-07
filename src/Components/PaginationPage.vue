80b169cead9630c669eca3fe14f4167fc24ea681
Amrilazim3 <a.azim0711@gmail.com>
2022-12-07 09:06:48 +0800

    emit event when pagination button is click

--- src/Components/PaginationPage.vue
+++ src/Components/PaginationPage.vue
@@ -12,7 +12,7 @@
 					v-else
 					class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-indigo-200 focus:border-primary focus:text-primary"
 					:class="{ 'bg-indigo-200': link['active'] }"
-					@click.prevent="paginateNext(link['url'])"
+					@click="$emit('paginateNext', link['url'])"
 					v-html="link['label']"
 				/>
 			</template>
@@ -22,16 +22,8 @@
 
 <script lang="ts" setup>
 import { defineProps } from "vue";
-import { usePostStore } from "@/stores/post";
 
 defineProps({
 	links: Object,
 });
-
-const post = usePostStore();
-
-const paginateNext = async (link: string) => {
-	await post.getPosts(link);
-	window.scrollTo(0, 0);
-};
 </script>
