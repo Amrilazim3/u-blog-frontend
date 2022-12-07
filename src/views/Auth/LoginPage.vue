e2002cd33de74e2ab1b2dfb4a85b2f0e0bf15b99
Amrilazim3 <a.azim0711@gmail.com>
2022-12-07 10:17:58 +0800

    enabled loading animation

--- src/views/Auth/LoginPage.vue
+++ src/views/Auth/LoginPage.vue
@@ -29,12 +29,24 @@
 						placeholder="Enter your password"
 						validation="required"
 					/>
-					<FormKit
+					<button
 						type="submit"
-						label="Login"
-						input-class="$reset w-full text-white p-3 border rounded-md bg-blue-500 hover:bg-blue-600"
-					/>
-					<p class="text-sm">
+						class="flex justify-center w-full text-white p-2.5 border rounded-md bg-blue-500 hover:bg-blue-600"
+					>
+						Login
+						<template v-if="isLoading">
+							<svg
+								class="animate-spin self-center h-5 w-5 fill-white ml-3"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 512 512"
+							>
+								<path
+									d="M304 48c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48s48-21.5 48-48zm0 416c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48s48-21.5 48-48zM48 304c26.5 0 48-21.5 48-48s-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48zm464-48c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48s48-21.5 48-48zM142.9 437c18.7-18.7 18.7-49.1 0-67.9s-49.1-18.7-67.9 0s-18.7 49.1 0 67.9s49.1 18.7 67.9 0zm0-294.2c18.7-18.7 18.7-49.1 0-67.9S93.7 56.2 75 75s-18.7 49.1 0 67.9s49.1 18.7 67.9 0zM369.1 437c18.7 18.7 49.1 18.7 67.9 0s18.7-49.1 0-67.9s-49.1-18.7-67.9 0s-18.7 49.1 0 67.9z"
+								/>
+							</svg>
+						</template>
+					</button>
+					<p class="text-sm mt-3">
 						don't have an account?
 						<router-link to="/register" class="text-blue-500"
 							>Register now</router-link
@@ -50,13 +62,24 @@
 import router from "@/router";
 import { useAuthStore } from "@/stores/auth";
 import { IonContent, IonPage } from "@ionic/vue";
+import { ref } from "vue";
 
 const auth = useAuthStore();
 
+const isLoading = ref(false);
+
 const login = async (data: any, node: any) => {
+	isLoading.value = true;
+
 	const loginRes = await auth.login(data, node);
 
+	if (loginRes == 422) {
+		isLoading.value = false;
+	}
+
 	if (loginRes == 200) {
+		isLoading.value = false;
+
 		router.push("/app");
 	}
 };
